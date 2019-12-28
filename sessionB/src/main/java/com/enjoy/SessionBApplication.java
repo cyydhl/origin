ea77eea23cc561a4f3d07dd200ad87b6baa86214
java_htkf <douhailong1992@163.com>
2019-12-28 21:42:18 +0800

    redis单点登录

---
+++ sessionB/src/main/java/com/enjoy/SessionBApplication.java
@@ -0,0 +1,13 @@
+package com.enjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionBApplication.class, args);
+	}
+
+}
